ffae40d3f319410da88bb67736b0e630d46ac30a
hemo-sawang <hemo.sawang2026@gmail.com>
2026-07-22 18:03:27 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE='hemostock-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='hemostock-v2';
+const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));
+});
